8036fd7817720644876aeaa10364dc05baf820c3
KolyanPie <mbilibin@mail.ru>
2021-05-03 18:39:29 +0300

    gradle init

---
+++ src/main/java/ru/kolyanpie/dlp/DLPApplication.java
@@ -0,0 +1,12 @@
+package ru.kolyanpie.dlp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DLPApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DLPApplication.class, args);
+    }
+}
